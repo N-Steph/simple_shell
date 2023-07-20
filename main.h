67d1c55a4624c0b55e2f723aeb69835493aca3e5
nouaffo <snouaffo@gmail.com>
2023-07-20 03:54:41 +0000

    Add prototype of _getenv and struct dir_node

--- main.h
+++ main.h
@@ -2,4 +2,10 @@
 #define MAIN_H
 int _strlen(const char *s);
 void print_dir_path(void);
+char *_getenv(const char *name);
+struct dir_node
+{
+	char *dir;
+	struct dir_node *next;
+};
 #endif /* MAIN_H */
