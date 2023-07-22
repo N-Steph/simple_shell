3c9e5b1c7ca908281e981ed42d5081ea7138d413
nouaffo <snouaffo@gmail.com>
2023-07-22 23:26:26 +0000

    Add header files

--- main.h
+++ main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+extern char **environ;
 int _strlen(const char *s);
 void print_dir_path(void);
 char *_getenv(const char *name);
@@ -8,4 +16,5 @@ struct dir_node
 	char *dir;
 	struct dir_node *next;
 };
+void handle_args(char **lineptr, char **agrs);
 #endif /* MAIN_H */
