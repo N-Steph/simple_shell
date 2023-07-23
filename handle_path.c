7b72888a36917889685bd79d58d5377399271ddc
nouaffo <snouaffo@gmail.com>
2023-07-23 19:11:04 +0000

    Handle the path of the argument passed to the shell

---
+++ handle_path.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * handle_path - handle the path of command line argument
+ * This function is going to check for the existence of
+ * a command
+ *
+ * @args: array of arguments
+ *
+ * Return: 0 (success), -1 (failure), 1(Success, full path passed)
+ */
+int handle_path(char **args)
+{
+	struct stat statbuf;
+	struct dir_node *node, *head;
+	char *mem;
+
+	if (args == NULL)
+		return (-1);
+	if (stat(args[0], &statbuf) != 0)
+	{
+		/* Put this code in a function */
+		node = linked_list_dir_path();
+		head = node;
+		while (node->next != NULL)
+		{
+			mem = malloc(sizeof(char) * (strlen(node->dir) + strlen(args[0]) + 2));
+			if (mem == NULL)
+				return (-1);
+			mem = memcpy(mem, node->dir, strlen(node->dir));
+			mem = strcat(mem, "/");
+			mem = strcat(mem, args[0]);
+			if (stat(mem, &statbuf) == 0)
+			{
+				free(head->dir);
+				free_list(head);
+				args[0] = mem;
+				return (0);
+			}
+			free(mem);
+			node = node->next;
+		}
+		free(head->dir);
+		free_list(head);
+	}
+	else
+	{
+		return (1);
+	}
+	return (-1);
+}
