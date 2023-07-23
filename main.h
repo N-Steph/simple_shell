b50da388c058b30dd8a13faf879d06a180d4a0c4
nouaffo <snouaffo@gmail.com>
2023-07-23 19:13:11 +0000

    Add function prototypes

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,4 +18,7 @@ struct dir_node
 	struct dir_node *next;
 };
 void handle_args(char **lineptr, char **agrs);
+int handle_path(char **agrs);
+struct dir_node *linked_list_dir_path(void);
+void free_list(struct dir_node *head);
 #endif /* MAIN_H */
