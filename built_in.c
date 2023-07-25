b7c61e977f638727d28c6c23a3266a20f908a0a4
nouaffo <snouaffo@gmail.com>
2023-07-25 04:44:01 +0000

    Handle exit status

--- built_in.c
+++ built_in.c
@@ -11,12 +11,27 @@
 void built_in(char **args, char *lineptr)
 {
 	char *cmd_built_in[2] = {"exit", "env"};
+	int status;
 
 	if (strcmp(args[0], cmd_built_in[0]) == 0)
 	{
-		free(lineptr);
-		printf("UUUUUUUUU\n");
-		exit(0);
+		if (args[1] == NULL || strcmp(args[1], "0") == 0)
+		{
+			free(lineptr);
+			exit(0);
+		}
+		else
+		{
+			status = atoi(args[1]);
+			if (status == 0)
+			{
+				printf("hsh : 1 : Illegal number : %s\n", args[1]);
+				free(lineptr);
+				exit(2);
+			}
+			free(lineptr);
+			exit(status);
+		}
 	}
 	else if (strcmp(args[0], cmd_built_in[0]) == 0)
 	{
